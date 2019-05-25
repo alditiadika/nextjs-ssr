5e8ef8dd086e84375500b08e92f27feb58df4125
Randika Alditia <randikaalditia@gmail.com>
2019-05-26 04:37:03 +0700

    server prepare

--- pages/components/main.js
+++ pages/components/main.js
@@ -2,14 +2,22 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
 import { Container, Card, CardBody } from 'reactstrap'
-
+const arr = new Array(10).fill(0)
 class Main extends Component {
     render() {
         return (
             <Container style={{ marginTop: '20px' }}>
                 <Card style={{ backgroundColor: 'gray' }}>
-                    <CardBody>test</CardBody>
-                </Card>{' '}
+                    <CardBody>
+                        <table>
+                            <tbody>
+                                {arr.map(item => (
+                                    <tr key={Math.random()}>{item}</tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </CardBody>
+                </Card>
             </Container>
         )
     }
